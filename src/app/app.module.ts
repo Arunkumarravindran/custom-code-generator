07130647d96c9edd552d10e16583525d2e60df97
user <arunkumarravindran@outlook.com>
2020-04-17 17:59:05 +0530

    popUp done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @NgModule({
    declarations: [
       AppComponent,
       JavaScreenComponent,
       DependencyScreenComponent
+   ],
+   entryComponents:[
+    DependencyScreenComponent
    ],
    imports: [
       BrowserModule,
@@ -26,6 +31,7 @@ import {MatIconModule} from '@angular/material/icon';
       MatToolbarModule,
       MatIconModule,
       MatCardModule,
+      MatDialogModule,
       MatCheckboxModule,
       MatFormFieldModule,
       MatInputModule,
--- src/component/java-screen/java-screen.component.ts
+++ src/component/java-screen/java-screen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import{DependencyScreenComponent} from 'src/component/dependency-screen/dependency-screen.component'
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-java-screen',
@@ -6,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./java-screen.component.css']
 })
 export class JavaScreenComponent implements OnInit {
+  checkedIndex = -1;
+  languages: any[] = [
+    {value: 'java', viewValue: 'Java'},
+    {value: 'kotlin', viewValue: 'Kotlin'},
+    {value: 'groovy', viewValue: 'Groovy'}
+  ];
 
-  constructor() { }
+  projects: any[] = [
+    {value: 'maven', viewValue: 'Maven Project'},
+    {value: 'gradle', viewValue: 'Gradle'}
+  ];
+
+  packaging: any[] = [
+    {value: 'jar', viewValue: 'JAR'},
+    {value: 'war', viewValue: 'WAR'}
+  ];
+
+  javaVersion: any[] = [
+    {value: '14', viewValue: '14'},
+    {value: '11', viewValue: '11'},
+    {value: '8', viewValue: '8'}
+  ];
+
+  springVersion: any[] = [
+    {value: '2.3.0 M4', viewValue: '2.3.0 M4'},
+    {value: '2.3.0 (SNAPSHOT)', viewValue: '2.3.0 (SNAPSHOT)'},
+    {value: '2.2.7 (SNAPSHOT)', viewValue: '2.2.7 (SNAPSHOT)'},
+    {value: '2.2.6', viewValue: '2.2.6'},
+    {value: '2.1.14 (SNAPSHOT)', viewValue: '2.1.14 (SNAPSHOT)'},
+    {value: '2.1.13', viewValue: '2.1.13'}
+  ];
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
+  checkboxChange(event: MatCheckboxChange, index: number) {
+    this.checkedIndex = event.checked ? index : -1;
+}
+  openDependency() {
+    const dialogRef = this.dialog.open(DependencyScreenComponent, {
+      width: '50%',
+   
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+}
 }
